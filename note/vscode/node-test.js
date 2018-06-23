d3cd62ecff27a8009dca0fa6790d8a9f99c06443
summerlius <summerliuz@163.com>
2018-06-23 20:27:18 +0800

    .

--- note/vscode/node-test.js
+++ note/vscode/node-test.js
@@ -1,2 +1,26 @@
-var msg = 'Hello World';
-console.log(msg);
+const fs = require('fs');
+
+function someAsyncOperation(callback) {
+    // Assume this takes 95ms to complete
+    fs.readFile('./note/special_characters.md', callback);
+}
+
+const timeoutScheduled = Date.now();
+
+setTimeout(() => {
+    const delay = Date.now() - timeoutScheduled;
+
+    console.log(`${delay}ms have passed since I was scheduled`);
+}, 100);
+
+
+// do someAsyncOperation which takes 95 ms to complete
+someAsyncOperation((err, data) => {
+    console.log('读取文件完成回调');
+    const startCallback = Date.now();
+
+    // do something that will take 10ms...
+    while (Date.now() - startCallback < 2000) {
+        // do nothing
+    }
+});
