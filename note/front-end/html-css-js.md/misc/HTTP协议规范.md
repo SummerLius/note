a876d61797c5d76d90c6d7cfe70e72aa92e6ecdf
summerlius <summerliuz@163.com>
2019-02-18 21:05:13 +0800

    .

---
+++ note/front-end/html-css-js.md/misc/HTTP协议规范.md
@@ -0,0 +1,9 @@
+
+## 链接
+
+- [MDN | HTTP介绍](https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Overview)
+- [MDN | HTTP资源与规范](https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Resources_and_specifications)
+- [Tutorialspoint | HTTP](https://www.tutorialspoint.com/http/http_overview.htm)
+- [博客 | 鲜为人知的HTTP协议头字段详解大全](https://zhuanlan.zhihu.com/p/34908942)
+- [IETF | rfc](https://tools.ietf.org/rfc/index)
+- [WIKI | HTTP](https://zh.wikipedia.org/wiki/%E8%B6%85%E6%96%87%E6%9C%AC%E4%BC%A0%E8%BE%93%E5%8D%8F%E8%AE%AE)
---
+++ note/test/node-server.js
@@ -0,0 +1,75 @@
+/**
+ * @description Nodejs HTTP Simple Server
+ */
+
+const url = require('url');
+const http = require('http');
+const qs = require('querystring');
+
+const serverCtrl = {
+    router: {
+        '/404': function(req, res) {
+            // response.setHeader('Content-Type', 'text/html');
+            // response.setHeader('Set-Cookie', ['type=ninja', 'language=javascript']);
+            res.writeHead(404, {
+            });
+            res.end(http.STATUS_CODES[404]);
+        },
+        '/api': function(req, res) {
+            res.writeHead(200, {
+                // 'Expires': 'Thu, 01 Dec 2020 16:00:00 GMT',
+                'Cache-Control': 'max-age=3600',
+                'Content-Type': 'application/json'
+            });
+
+            let ret = {
+                code: 200,
+                success: true
+            }
+
+            res.end(JSON.stringify(ret));
+        },
+        '/index.html': function(req, res) {
+            let str = 
+            `<html>
+              <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <title>Thimble Sample</title>
+              </head>
+              <body>
+                <h1>Welcome to Thimble</h1>
+                <p>
+                  Make something <strong>amazing</strong> with the web!
+                </p>
+              </body>
+              <script src="/api"></script>
+            </html>`;
+
+            res.writeHead(200, {
+                // 'Expires': 'Thu, 01 Dec 2020 16:00:00 GMT',
+                'Cache-Control': 'max-age=3600',
+                'Content-Type': 'text/html; charset=utf-8'
+            });
+
+            res.end(str);
+        }
+    },
+    server: http.createServer((req, res) => {
+        let urlObj = url.parse(req.url);
+        let pathname = urlObj.pathname;
+        req.query = qs.parse(urlObj.query);
+
+        serverCtrl.router[pathname]
+            ? serverCtrl.router[pathname](req, res)
+            : serverCtrl.router['/404'](req, res);
+    }),
+    startServer: function() {
+        this.server.listen(8888, '0.0.0.0', () => {
+            console.log('start server...');
+        });
+    }
+};
+
+serverCtrl.startServer();
+
