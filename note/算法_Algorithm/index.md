0e2d4d753b22d611102bceb5c5883cb249ae0a7d
summerlius <summerliuz@163.com>
2019-03-20 17:55:21 +0800

    .

--- note/算法_Algorithm/index.md
+++ note/算法_Algorithm/index.md
@@ -5,6 +5,31 @@
 - 排序算法可以分为两类：
     - 原地排序算法：除了函数调用所需要的栈和固定数目的实例变量之外无需额外的内存
     - 非原地排序算法：
+- 排序算法按稳定分类：
+    - 稳定：冒泡、插入、归并、基数、计数、桶
+    - 不稳定：选择、堆排、快排、希尔
+        - 分析：
+            - 简单选择排序不稳定：例如 “5 8 5 2 9”，第一次循环，第一个 5 会和 2 交换，此时两个 5 的相对顺序就改变了；
+- 其它
+    - 最高位优先（MSD，Most Significant Digit first）
+    - 最低位优先（LSD）
+
+
+|排序算法|平均复杂度|最好情况|最坏情况|空间复杂度|排序方式|稳定性|其它|
+|:--:|:--:|:--:|:--:|:--:|:--:|:--:|:--:|
+|冒泡排序|O(n²)|O(n)|O(n²)|O(1)|in-place|稳定||
+|选择排序|O(n²)|O(n²)|O(n²)|O(1)|in-place|不稳定||
+|插入排序|O(n²)|O(n)|O(n²)|O(1)|in-place|稳定||
+|快速排序||||||||
+|堆排序||||||||
+|希尔排序||||||||
+|归并排序||||||||
+|基数排序|O(n*k)|O(n*k)|O(n*k)|O(n)|out-place|稳定|k 为数组中最大数值的位数，所以该<br/>值不大，一般可控，例如 999 对应 k 为 3|
+|计数排序|O(n + k)|O(n + k)|O(n + k)|O(k)|out-place|稳定|k 为桶的个数，该桶个数与数组长度<br/>相关，与数组里面最大值和最小值相关|
+|桶排序||||||||
+|||||||||
+
+
 
 ## 链接
 
@@ -12,3 +37,9 @@
 - https://mp.weixin.qq.com/s/vn3KiV-ez79FmbZ36SX9lg
 - https://blog.csdn.net/justloveyou_/article/details/72730597
 - https://visualgo.net/zh
+- [中文WIKI 算法](https://zh.wikipedia.org/wiki/%E7%AE%97%E6%B3%95)
+- [中文WIKI 排序算法](https://zh.wikipedia.org/wiki/%E6%8E%92%E5%BA%8F%E7%AE%97%E6%B3%95)
+- [英文WIKI 排序算法](https://en.wikipedia.org/wiki/Sorting_algorithm)
+- [排序算法动画](https://www.toptal.com/developers/sorting-algorithms)
+- [算法基础 网易课程](https://www.coursera.org/learn/suanfa-jichu)
+- https://github.com/jeffgerickson/algorithms
--- note/算法_Algorithm/算法_Algorithms/BaseSort.js
+++ note/算法_Algorithm/算法_Algorithms/BaseSort.js
@@ -15,7 +15,8 @@ module.exports = {
     shellSort,
 
     // 归并
-    mergeSort,
+    mergeSortTopDown,
+    mergeSortBottomUp,
 
     // 基数
     radixSort,
@@ -184,7 +185,42 @@ function selectSort(originArray) {
  * @param {Array<Integer>} originArray
  * @returns {Array<Integer>}
  */
-function heapSort(originArray) {}
+function heapSort(originArray) {
+    let len = arr.length;
+
+    let heapify = function (
+        arr // 待排序的数组
+        , x // 元素的下标
+        , len // 数组的长度
+    ) {
+        let l = 2 * x + 1;
+        let r = 2 * x + 2;
+        let largest = x;
+
+        if (l < len && arr[l] > arr[largest]) {
+            largest = l;
+        }
+
+        if (r < len && arr[r] > arr[largest]) {
+            largest = r;
+        }
+
+        if (largest !== x) {
+            swap(x, largest, arr);
+            heapify(arr, largest, len);
+        }
+    }
+
+    for (let i = Math.floor(len / 2); i >= 0; i--) {
+        heapify(arr, i, len);
+    }
+
+    for (let i = len - 1; i >= 1; i--) {
+        swap(0, i, arr);
+        heapify(arr, 0, --len);
+    }
+    return arr;
+}
 
 /**
  * @description 插入排序>直接插入排序
@@ -219,25 +255,265 @@ function insertSort(originArray) {
 
 /**
  * @description 插入排序>希尔排序
+ * @param {Array<Integer>} originArray
+ * @returns {Array<Integer>}
  */
 function shellSort() {
+    let len = arr.length,
+        temp,
+        gap = 1;
+
+    while (gap < len / 3) {
+        gap = gap * 3 + 1;
+    }
+
+    for (gap; gap > 0; gap = Math.floor(gap / 3)) {
+        for (let i = gap; i < len; i++) {
+            temp = arr[i];
+            for (var j = i - gap; j >= 0 && arr[j] > temp; j -= gap) {
+                arr[j + gap] = arr[j];
+            }
+            arr[j + gap] = temp;
+        }
+    }
+    return arr;
+
+
+    // sort(originalArray) {
+    //     // Prevent original array from mutations.
+    //     const array = [...originalArray];
+
+    //     // Define a gap distance.
+    //     let gap = Math.floor(array.length / 2);
+
+    //     // Until gap is bigger then zero do elements comparisons and swaps.
+    //     while (gap > 0) {
+    //         // Go and compare all distant element pairs.
+    //         for (let i = 0; i < (array.length - gap); i += 1) {
+    //             let currentIndex = i;
+    //             let gapShiftedIndex = i + gap;
+
+    //             while (currentIndex >= 0) {
+    //                 // Call visiting callback.
+    //                 this.callbacks.visitingCallback(array[currentIndex]);
+
+    //                 // Compare and swap array elements if needed.
+    //                 if (this.comparator.lessThan(array[gapShiftedIndex], array[currentIndex])) {
+    //                     const tmp = array[currentIndex];
+    //                     array[currentIndex] = array[gapShiftedIndex];
+    //                     array[gapShiftedIndex] = tmp;
+    //                 }
+
+    //                 gapShiftedIndex = currentIndex;
+    //                 currentIndex -= gap;
+    //             }
+    //         }
+
+    //         // Shrink the gap.
+    //         gap = Math.floor(gap / 2);
+    //     }
+
+    //     // Return sorted copy of an original array.
+    //     return array;
+    // }
+}
+
+/**
+ * @description 归并排序>自顶向下（递归）
+ * @param {Array<Integer>} originArray
+ * @returns {Array<Integer>}
+ */
+function mergeSortTopDown(originArray) {
+    let len = originArray.length;
+
+    if (len < 2) {
+        return arr;
+    }
+
+    let middleIndex = Math.floor(len / 2); // 获取中间元素的索引
+    let left = arr.slice(0, middleIndex); // 获取左半部分的元素
+    let right = arr.slice(middleIndex); // 获取右半部分的元素
+
+    let merges = function (left, right) {
+        // 保存结果的数组
+        let result = [];
+
+        while (left.length && right.length) {
+            if (left[0] < right[0]) {
+                result.push(left.shift())
+            } else {
+                result.push(right.shift())
+            }
+        }
+
+        // 如果左半边还有元素
+        while (left.length) {
+            result.push(left.shift());
+        }
+
+        // 如果右半边还有元素
+        while (right.length) {
+            result.push(right.shift());
+        }
+
+        return result;
+    }
+
+    return merges(merge(left), merge(right));
+}
+
+/**
+ * @description 归并排序>自底向上（迭代）
+ * @param {Array<Integer>} originArray
+ * @returns {Array<Integer>}
+ */
+function mergeSortBottomUp(originArray) {
 
 }
 
 /**
- * @description 归并排序
- */
-function mergeSort() {}
-
-/**
  * @description 基数排序
+ * @param {Array<Integer>} originArray
+ * @returns {Array<Integer>}
  */
-function radixSort() { }
+function radixSort(originArray) {
+    let len = originArray.length;
+
+    let max = Math.max(...originArray);
+    let maxLength = `${max}`.length;
+
+    let mod = 1
+    let buckets = []
+
+    while (maxLength > 0) {
+        mod *= 10;
+
+        for (let i = 0; i < len; i++) {
+            let modValue = originArray[i] % mod;
+            !buckets[modValue] && (buckets[modValue] = []);
+            buckets[modValue].push(originArray[i]);
+        }
+
+        originArray.length = 0; // 将原数组清空
+        for (let i = 0; i < buckets.length; i++) {
+            if (buckets[i]) {
+                for (let j = 0; j < buckets[i].length; j++) {
+                    originArray.push(buckets[i][j]);
+                }
+            }
+        }
+        buckets = [];
+        maxLength--;
+    }
+    return originArray;
+}
 
 /**
  * @description 计数排序
+ * 
+ * @param {Array<Integer>} originArray
+ * @returns {Array<Integer>}
  */
-function countSort() { }
+function countSort(originArray) {
+    let index = 0;
+    let len = originArray.length;
+    let min = Math.min.apply(null, arr); // 最小值
+    let max = Math.max.apply(null, arr); // 最大值
+    let buckets = []; // 结果数组
+
+    // 向新数组中填充0
+    for (let i = min; i <= max; i++) {
+        buckets[i] = 0;
+    }
+
+    // 把各个数组中对应的元素计数加一
+    for (let i = 0; i < len; i++) {
+        buckets[originArray[i]]++;
+    }
+    // 按照计数的元素进行排序
+    for (let i = min; i <= max; i++) {
+        while (buckets[i]-- > 0) {
+            originArray[index++] = i;
+        }
+    }
+    return originArray;
+}
+
+// // 桶排序
+// function bucketSort(arr, bucketSize) {
+//     if (arr.length === 0) {
+//         return arr;
+//     }
+
+//     var i;
+//     var minValue = arr[0];
+//     var maxValue = arr[0];
+//     for (i = 1; i < arr.length; i++) {
+//         if (arr[i] < minValue) {
+//             minValue = arr[i];                // 输入数据的最小值
+//         } else if (arr[i] > maxValue) {
+//             maxValue = arr[i];                // 输入数据的最大值
+//         }
+//     }
+
+//     //桶的初始化
+//     var DEFAULT_BUCKET_SIZE = 5;            // 设置桶的默认数量为5
+//     bucketSize = bucketSize || DEFAULT_BUCKET_SIZE;
+//     var bucketCount = Math.floor((maxValue - minValue) / bucketSize) + 1;
+//     var buckets = new Array(bucketCount);
+//     for (i = 0; i < buckets.length; i++) {
+//         buckets[i] = [];
+//     }
+
+//     //利用映射函数将数据分配到各个桶中
+//     for (i = 0; i < arr.length; i++) {
+//         buckets[Math.floor((arr[i] - minValue) / bucketSize)].push(arr[i]);
+//     }
+
+//     arr.length = 0;
+//     for (i = 0; i < buckets.length; i++) {
+//         insertionSort(buckets[i]);                      // 对每个桶进行排序，这里使用了插入排序
+//         for (var j = 0; j < buckets[i].length; j++) {
+//             arr.push(buckets[i][j]);
+//         }
+//     }
+
+//     return arr;
+// }
+
+// // 桶排序 -- 不改变原数组
+// function bucket(arr, size = 5) {
+//     let len = arr.length;
+//     if (len < 2) {
+//         return arr;
+//     }
+
+//     // 获取最大值和最小值
+//     const max = Math.max.apply(null, arr);
+//     const min = Math.min.apply(null, arr);
+
+//     // 计算出桶的数量  size是截距
+//     const bucketCount = Math.floor((max - min) / size) + 1;
+//     // 根据桶的个数创建指定长度的数组
+//     const buckets = new Array(bucketCount);
+//     // 将每个桶塞到大桶里面去
+//     for (let i = 0; i < bucketCount; i++) {
+//         buckets[i] = [];
+//     }
+//     // 利用映射函数将数据分配到各个桶里面去
+//     for (let i = 0; i < arr.length; i++) {
+//         // 逢size进1
+//         let index = Math.floor((arr[i] - min) / size);
+//         buckets[index].push(arr[i]);
+//     }
+//     //对每个桶中的数据进行排序--借助于快速排序算法
+//     for (let i = 0; i < buckets.length; i++) {
+//         buckets[i] = quick(buckets[i]);
+//     }
+
+//     // flatten数组--有点不足就是会将原数组中的String改变为Number
+//     return buckets.join(',').split(',').filter(v => v !== '').map(Number);
+// }
 
 
 
